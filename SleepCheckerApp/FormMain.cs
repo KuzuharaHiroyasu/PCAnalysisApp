7043c51d61c2accf56015719ef9ee1c7ef686e1d
KuzuharaHiroyasu <43770961+KuzuharaHiroyasu@users.noreply.github.com>
2018-11-20 17:25:24 +0900

    アラームファイルの参照をC:\\audio配下に指定 （スタートアップだとWindows起動時に再生が自動で開始されるし、隠しファイルにするとファイルが見つからないエラーとなるため）

--- SleepCheckerApp/FormMain.cs
+++ SleepCheckerApp/FormMain.cs
@@ -85,6 +85,7 @@ namespace SleepCheckerApp
         private const string SOUND_1000HZ = "1000Hz.wav";
         private const string SOUND_5000HZ = "5000Hz.wav";
         private const string SOUND_10000HZ = "10000Hz.wav";
+        private const string AUDIO_PATH = "C:\\audio\\";
 
         enum alarmIndex
         {
@@ -296,7 +297,7 @@ namespace SleepCheckerApp
 
             comboBox_alarm.SelectedIndex = 0;
 
-            player = new System.Media.SoundPlayer(SoundFile);
+            player = new System.Media.SoundPlayer(AUDIO_PATH + SoundFile);
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
@@ -1620,7 +1621,7 @@ namespace SleepCheckerApp
                     SoundFile = SOUND_1000HZ;
                     break;
             }
-            player = new System.Media.SoundPlayer(SoundFile);
+            player = new System.Media.SoundPlayer(AUDIO_PATH + SoundFile);
 
             if(player != null)
             {
